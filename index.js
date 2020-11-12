9f17506bdd655bca8fd7f688e8ed252ac8fa7413
zfinnan <63361320+zfinnan@users.noreply.github.com>
2020-11-12 14:02:01 -0600

    added myModule

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { add, subtract } = require("./myModule");
+
+
 let name = 'Zach Finnan';
 console.log(name);
 
@@ -6,3 +9,6 @@ function printName(person) {
 }
 
 console.log(printName(name));
+
+console.log(add(5, 50));
+console.log(subtract(10,20));
---
+++ myModule.js
@@ -0,0 +1,15 @@
+const beBasic = () => "That's so fetch!"
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num2 - num1;
+}
+
+module.exports = {
+    beBasic,
+    add,
+    subtract
+}
